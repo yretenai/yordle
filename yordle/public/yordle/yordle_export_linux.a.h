b49734aa8d985c2dae3b73c4d1da0ba77c2e2bab
Lilith Naomi Nightingale <yretenai@gmail.com>
2021-04-23 21:54:41 +0100

    [project] static library for linux (todo: move to option?)

--- yordle/public/yordle/yordle_export_linux.a.h
+++ yordle/public/yordle/yordle_export_linux.a.h
@@ -9,15 +9,15 @@
 #  ifndef YORDLE_EXPORT
 #    ifdef yordle_EXPORTS
         /* We are building this library */
-#      define YORDLE_EXPORT __attribute__((visibility("default")))
+#      define YORDLE_EXPORT 
 #    else
         /* We are using this library */
-#      define YORDLE_EXPORT __attribute__((visibility("default")))
+#      define YORDLE_EXPORT 
 #    endif
 #  endif
 
 #  ifndef YORDLE_NO_EXPORT
-#    define YORDLE_NO_EXPORT __attribute__((visibility("hidden")))
+#    define YORDLE_NO_EXPORT 
 #  endif
 #endif
 
